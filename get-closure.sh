a0267ce77cb869c585d2733b1772d374dfd81b40
Michael Su <mdasu1@gmail.com>
2012-11-05 15:15:43 -0800

    Initial commit

---
+++ get-closure.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+set -eu
+
+curl http://closure-templates.googlecode.com/files/closure-templates-for-javascript-latest.zip > soy.zip
+unzip -p soy.zip SoyToJsSrcCompiler.jar > soy.jar
+mv soy.jar closure/soy.jar
+rm soy.zip
