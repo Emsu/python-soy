4dcf757e4fcd46a5cf240adef9a026013d874d2b
Michael Su <mdasu1@gmail.com>
2012-11-05 18:06:37 -0800

    modified manifest and shell script to match modification of package directory

--- get-closure.sh
+++ get-closure.sh
@@ -3,5 +3,5 @@ set -eu
 
 curl http://closure-templates.googlecode.com/files/closure-templates-for-javascript-latest.zip > soy.zip
 unzip -p soy.zip SoyToJsSrcCompiler.jar > soy.jar
-mv soy.jar closure/soy.jar
+mv soy.jar closure_soy/soy.jar
 rm soy.zip
