11bc2d3a455b304a15eb6c0be326ed97014cc0c2
Michael Su <mdasu1@gmail.com>
2012-11-05 16:01:32 -0800

    changed version number to date soy compiler was last updated

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     long_description=open('README.md').read(),
     author='Michael Su',
     author_email='mdasu1@gmail.com',
-    version="0.1",
+    version="20111222",
     url="http://pypi.python.org/pypi/closure-soy",
     license='BSD',
     packages=find_packages(),
